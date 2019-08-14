e950290324a67e5286e2300f41a367d15fa0475b
goddy <wuchuansheng@yeah.net>
2019-08-14 20:49:16 +0800

    init

---
+++ extra.js
@@ -0,0 +1,5 @@
+var link = document.createElement('link');
+link.type = 'image/x-icon';
+link.rel = 'shortcut icon';
+link.href = 'icon.ico';
+document.getElementsByTagName('head')[0].appendChild(link);
